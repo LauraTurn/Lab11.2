f9f9f29cb30ca52485382de23f31a5140010d7d7
Laura Turner <lauraturner@quickenloans.com>
2021-05-21 09:55:27 -0400

    second

---
+++ 11.2CoffeeRegistration2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _11._2CoffeeRegistration2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 11.2CoffeeRegistration2/Models/NUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _11._2CoffeeRegistration2.Models
+{
+    public class NUser
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string confirmpassword { get; set; }
+        public int age { get; set; }
+        public string phonenumber { get; set; }
+
+        public static List<NUser> AllNUser = new List<NUser>();
+    }
+}
